7f8583425f4ac6e4d6625b857939cced3da0929d
jesus241295 <jesusgonvale@gmail.com>
2023-04-27 11:59:08 -0500

    styles are added in the Characters component with tailwind css

--- src/components/Characters/Characters.jsx
+++ src/components/Characters/Characters.jsx
@@ -10,21 +10,31 @@ const Characters = () => {
 
   return (
     <>
-      <div className="content">
-        <h1>Rick And Morty</h1>
+      <div className="flex bg-black h-80 items-center justify-center ">
+        <h1 className="text-orange-500 hover:text-white text-8xl font-bold">
+          Rick And Morty
+        </h1>
       </div>
-      <div className="card">
+      <div className="flex flex-wrap justify-center">
         {characters != null
           ? characters.map((character) => (
-              <div className="content-personaje" key={character.id}>
-                <div className="content-title">
-                  <a href={`/characters/${character.id}`}>{character.name}</a>
+              <div
+                className="w-72 m-4 bg-stone-700 rounded-xl"
+                key={character.id}
+              >
+                <div className="text-white text-center p-1 font-mono">
+                  <a
+                    className="text-xl hover:text-orange-500 font-bold"
+                    href={`/characters/${character.id}`}
+                  >
+                    {character.name}
+                  </a>
                 </div>
-                <div className="content-card-image">
+                <div>
                   <img
                     src={character.image}
                     alt="#"
-                    className="imagen-inicio"
+                    className="h-64 w-72 rounded-b-xl"
                   />
                 </div>
               </div>
