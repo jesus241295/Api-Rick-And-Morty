9c25c8ec61cc9670504a0faa58faf34d7fb0a5f8
jesus241295 <jesusgonvale@gmail.com>
2023-04-27 20:08:56 -0500

    breakpoints for mobile are added in the Characters component

--- src/components/Characters/Characters.jsx
+++ src/components/Characters/Characters.jsx
@@ -10,37 +10,36 @@ const Characters = () => {
 
   return (
     <>
-      <div className="flex bg-black h-96 items-center justify-center">
-        <h1 className="text-orange-500 hover:text-white text-8xl font-bold">
-          Rick And Morty
+      <div className="flex h-32 bg-black sm:h-80 items-center justify-center">
+        <h1 className="text-3xl text-orange-500 hover:text-white sm:text-7xl font-bold">
+          The Rick And Morty
         </h1>
       </div>
       <div className="flex flex-wrap justify-center ">
         {characters !== null ? (
           characters.map((character) => (
             <div
-              className="w-72 m-4 bg-stone-700 rounded-xl"
+              className="sm:w-72 m-4 bg-stone-700 rounded-xl"
               key={character.id}
             >
               <div className="text-white text-center p-1 font-mono">
                 <a
-                  className="text-xl hover:text-orange-500 font-bold"
+                  className="text-lg sm:text-xl hover:text-orange-500 font-bold"
                   href={`/characters/${character.id}`}
                 >
                   {character.name}
                 </a>
               </div>
-              <div>
-                <img
-                  src={character.image}
-                  alt="#"
-                  className="h-64 w-72 rounded-b-xl"
-                />
-              </div>
+
+              <img
+                src={character.image}
+                alt="#"
+                className="h-52 sm:h-64 w-72 rounded-b-xl"
+              />
             </div>
           ))
         ) : (
-          <div className="bg-stone-700 w-full h-screen text-center text-6xl font-sans font-bold text-white">
+          <div className="text-base pt-12 bg-stone-700 w-full h-screen text-center sm:text-4xl font-sans font-bold text-white">
             Sorry, No Hay Personajes....!
           </div>
         )}
