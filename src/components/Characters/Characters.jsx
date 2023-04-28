e527b504bfb99ff1070c05a5bf73b9c29174ab0b
jesus241295 <jesusgonvale@gmail.com>
2023-04-28 18:10:45 -0500

    the cards on the home screen are modified

--- src/components/Characters/Characters.jsx
+++ src/components/Characters/Characters.jsx
@@ -10,39 +10,36 @@ const Characters = () => {
 
   return (
     <>
-      <div className="flex h-32 bg-black sm:h-80 items-center justify-center">
-        <h1 className="text-3xl text-orange-500 hover:text-white sm:text-7xl font-bold">
-          The Rick And Morty
-        </h1>
-      </div>
-      <div className="flex flex-wrap justify-center ">
-        {characters !== null ? (
-          characters.map((character) => (
-            <div
-              className="sm:w-72 m-4 bg-stone-700 rounded-xl"
-              key={character.id}
-            >
-              <div className="text-white text-center p-1 font-mono">
-                <a
-                  className="text-lg sm:text-xl hover:text-orange-500 font-bold"
-                  href={`/characters/${character.id}`}
-                >
-                  {character.name}
-                </a>
+      <div className="bg-stone-700 w-full h-full 2xl:h-screen">
+        <div className="flex items-center justify-center font-bold text-orange-500 text-3xl h-32 bg-black sm:h-60 2xl:h-80">
+          <h1 className=" hover:text-white sm:text-7xl ">The Rick And Morty</h1>
+        </div>
+        <div className="flex flex-wrap justify-center">
+          {characters !== null ? (
+            characters.map((character) => (
+              <div
+                className="grid grid-cols-4 bg-stone-800 mt-2 border rounded-md border-gray-500 w-80 h-18 sm:w-66 m-2"
+                key={character.id}
+              >
+                <div className="flex items-center col-span-1 h-24 pl-1">
+                  <img src={character.image} alt="#" className="rounded-full" />
+                </div>
+                <div className="flex items-center col-span-3 pl-3 text-xl text-orange-500 font-bold font-sans ">
+                  <a
+                    href={`/characters/${character.id}`}
+                    className="hover:text-white"
+                  >
+                    {character.name}
+                  </a>
+                </div>
               </div>
-
-              <img
-                src={character.image}
-                alt="#"
-                className="h-52 sm:h-64 w-72 rounded-b-xl"
-              />
+            ))
+          ) : (
+            <div className="text-base pt-12 bg-stone-700 w-full h-screen text-center sm:text-4xl font-sans font-bold text-white">
+              ¡Sorry. No hay personajes...!
             </div>
-          ))
-        ) : (
-          <div className="text-base pt-12 bg-stone-700 w-full h-screen text-center sm:text-4xl font-sans font-bold text-white">
-            Sorry, No Hay Personajes....!
-          </div>
-        )}
+          )}
+        </div>
       </div>
     </>
   );
